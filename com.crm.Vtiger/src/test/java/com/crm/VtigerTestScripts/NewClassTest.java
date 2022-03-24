702cedf3a6ed4ce4652320a71b292bb4de4895fd
shree <shree@shree-PC>
2022-03-24 17:43:49 +0530

    modified

---
+++ com.crm.Vtiger/src/test/java/com/crm/VtigerTestScripts/NewClassTest.java
@@ -0,0 +1,12 @@
+package com.crm.VtigerTestScripts;
+
+import org.testng.annotations.Test;
+
+public class NewClassTest {
+	@Test
+	public void newclass()
+	{
+		System.out.println("New class created");
+	}
+
+}
