db794d5ce85777fa93fc08d42856db0dcc2a0a0a
shrut <shrut@LAPTOP-00UDRC2M>
2022-03-24 15:59:19 +0530

    Created new class

---
+++ com.crm.Vtiger/src/test/java/com/crm/VtigerTestScripts/ModifiedOrg.java
@@ -0,0 +1,12 @@
+package com.crm.VtigerTestScripts;
+
+import org.testng.annotations.Test;
+
+public class ModifiedOrg {
+	@Test
+	public void modifiedorg() {
+		
+		System.out.println("modified org");
+	}
+
+}
